07045ad8267f8e903e3fce98cd9a24555b59c3d1
jordy <marcelo2005jmsp@gmail.com>
2025-02-05 13:19:00 -0500

    repositorio creado

---
+++ SistemaGestionTarea/src/main/java/ec/edu/espoch/sistemagestiontarea/SistemaGestionTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sistemagestiontarea;
+
+/**
+ *
+ * @author jordy
+ */
+public class SistemaGestionTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
