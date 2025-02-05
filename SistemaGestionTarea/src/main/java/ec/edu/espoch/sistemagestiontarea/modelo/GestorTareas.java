3f866b1db4284d4e9b26f8809f9facf3aa1f2f3c
jordy <marcelo2005jmsp@gmail.com>
2025-02-05 13:43:43 -0500

    modelo creado

---
+++ SistemaGestionTarea/src/main/java/ec/edu/espoch/sistemagestiontarea/modelo/GestorTareas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.sistemagestiontarea.modelo;
+
+/**
+ *
+ * @author jordy
+ */
+public class GestorTareas {
+
+    public String agregarTarea(Tarea tarea) {
+        return "";
+    }
+
+    public String listarTareasPendientes() {
+        return "";
+    }
+
+    public String listarTareasCompletadas() {
+        return "";
+    }
+
+    public String marcarComoCompletada(int Id) {
+        return "";
+    }
+}
---
+++ SistemaGestionTarea/src/main/java/ec/edu/espoch/sistemagestiontarea/modelo/Tarea.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.sistemagestiontarea.modelo;
+
+/**
+ *
+ * @author jordy
+ */
+public class Tarea {
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private boolean tareaCompleta;
+
+    public Tarea(int id, String titulo, String descripcion, boolean tareaCompleta) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.tareaCompleta = tareaCompleta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isTareaCompleta() {
+        return tareaCompleta;
+    }
+
+    public void setTareaCompleta(boolean tareaCompleta) {
+        this.tareaCompleta = tareaCompleta;
+    }
+    
+    
+}
