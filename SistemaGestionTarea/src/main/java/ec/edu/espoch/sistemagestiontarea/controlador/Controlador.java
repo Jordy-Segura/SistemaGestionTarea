c189d19103fdbbd5ff20b8b962154707e85afd3d
Karen-404 <tat.3012.mc@gmail.com>
2025-02-06 11:54:35 -0500

    hola

---
+++ SistemaGestionTarea/src/main/java/ec/edu/espoch/sistemagestiontarea/controlador/Controlador.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.sistemagestiontarea.controlador;
+
+import ec.edu.espoch.sistemagestiontarea.modelo.GestorTareas;
+import ec.edu.espoch.sistemagestiontarea.vista.Interfaz;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+
+public class Controlador {
+private Interfaz vista;
+private GestorTareas modelo;
+
+    public Controlador(Interfaz vista, GestorTareas modelo) {
+        this.vista = vista;
+        this.modelo = new GestorTareas();
+    }
+    public void ProcesoGestorTareas (){
+        String titulo,descripcion;
+        
+        try {
+            titulo= vista.getTitle();
+            
+        }
+    }
+
+
+}
